dbea5214098b593a6f11f1fa8053b5d85f8e8719
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-04 21:29:26 +0700

    Refactor noise module to use SimpleNoise class

--- docs/noise.js
+++ docs/noise.js
@@ -1,13 +1,8 @@
-let noise = (() => {
-    function rand(x, y) {
-        return Math.sin(x * 12.9898 + y * 78.233) * 43758.5453 % 1;
-    }
+// SIMPLE NOISE FALLBACK
+class SimpleNoise {
+  noise2D(x, z) {
+    return Math.sin(x * 12.9898 + z * 78.233) * 43758.5453 % 1;
+  }
+}
 
-    function simplex2(x, y) {
-        return rand(x, y);
-    }
-
-    return { simplex2 };
-})();
-
-window.noise = noise;
+window.noise = new SimpleNoise();
