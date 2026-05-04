c42869f3a96f070d33c9852af2703a5d001e3b0a
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-04 21:38:42 +0700

    Update noise.js

--- docs/noise.js
+++ docs/noise.js
@@ -1,8 +1,8 @@
-// SIMPLE NOISE FALLBACK
-class SimpleNoise {
-  noise2D(x, z) {
-    return Math.sin(x * 12.9898 + z * 78.233) * 43758.5453 % 1;
+if (!window.noise) {
+  class SimpleNoise {
+    noise2D(x, z) {
+      return Math.sin(x * 12.9898 + z * 78.233) * 43758.5453 % 1;
+    }
   }
+  window.noise = new SimpleNoise();
 }
-
-window.noise = new SimpleNoise();
