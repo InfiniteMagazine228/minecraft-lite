2826f95473d9b9fe21c3804309f163879f84cfef
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-04 21:21:54 +0700

    Update ui.js

--- docs/ui.js
+++ docs/ui.js
@@ -1,7 +1,14 @@
-function startSingle(){
-  initGame(false);
-}
+let fps=0,frames=0,last=performance.now();
 
-function startMulti(){
-  initGame(true);
+function updateFPS(){
+    frames++;
+    let now = performance.now();
+
+    if(now-last>=1000){
+        fps=frames;
+        frames=0;
+        last=now;
+
+        document.getElementById("fps").innerText="FPS: "+fps;
+    }
 }
