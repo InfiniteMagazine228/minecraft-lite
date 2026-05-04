1912a2e9a78a7a1048e5115aaae901a5db357e5f
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-04 19:04:30 +0700

    Update network.js

--- docs/network.js
+++ docs/network.js
@@ -1,6 +1,18 @@
-let ws = new WebSocket("ws://localhost:3000");
+let ws;
 let players = {};
 
-ws.onmessage = e => {
-  players = JSON.parse(e.data);
-};
+function initNetwork(enable){
+  if(!enable) return;
+
+  ws = new WebSocket("ws://localhost:3000");
+
+  ws.onmessage = e=>{
+    players = JSON.parse(e.data);
+  };
+}
+
+function updateNetwork(camera){
+  if(ws && ws.readyState===1){
+    ws.send(JSON.stringify(camera.position));
+  }
+}
